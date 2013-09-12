887ce88e5c1a9e50f48b38fb95ffaeaaaea3238c
Pawan Mittal <pawan_mittal@condenast.com>
2013-09-12 16:40:12 -0700

    adding logic to insert expenses

--- app/Controller/FileUploadController.php
+++ app/Controller/FileUploadController.php
@@ -1,9 +1,37 @@
 <?php
 
+require_once '../DTO/ExpenseDTO.php';
 
 class FileUploadController extends AppController {
 
+    var $uses = array('Expense');
+
     public function index() {
         $this->view = '/ExpenseTracker/fileUpload';
+        $file = '../Test/Client/daily_expenses.csv';
+        $expenseObjects = $this->getFileContents($file);
+        foreach($expenseObjects as $expenseObject) {
+            $expenseDTO = new ExpenseDTO();
+            $expenseDTO->updateExpenseDTOFromStdObject($expenseObject);
+            $expenseId = $this->Expense->saveOrUpdate($expenseDTO);
+            print_r('Expense saved with id ' . $expenseId . "<BR>");
+        }
+    }
+
+    private function getFileContents($file) {
+        $expenseObjects = array();
+        if (($handle = fopen($file, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $expenseObject = new StdClass();
+                $expenseObject->date = $data[0];
+                $expenseObject->category = $data[1];
+                $expenseObject->subCategory = $data[2];
+                $expenseObject->vendor = $data[3];
+                $expenseObject->description = $data[4];
+                $expenseObject->amount = $data[5];
+                array_push($expenseObjects, $expenseObject);
+            }
+        }
+        return $expenseObjects;
     }
 }
