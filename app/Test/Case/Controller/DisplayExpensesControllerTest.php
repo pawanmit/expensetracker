b41b605730f9d2c524fe39dd2b896e5b66b8e454
Pawan Mittal <pawan_mittal@condenast.com>
2013-09-13 15:26:01 -0700

    adding new test case class

---
+++ app/Test/Case/Controller/DisplayExpensesControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+App::uses('AppController', 'Controller');
+App::uses('Expense', 'Model');
+
+class DisplayExpensesControllerTest  extends ControllerTestCase {
+    public $fixtures = array('app.expense');
+
+    public function testExpenseTotalOfCategoryByMonth() {
+
+    }
+}
